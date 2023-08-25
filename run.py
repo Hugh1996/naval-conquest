47ee843a41343ccfe9d47787bddd4aa3b9124b16
Hugh1996 <>
2023-08-25 20:17:40 +0000

    Add def __init__ function in grid class

--- run.py
+++ run.py
@@ -8,3 +8,13 @@ class Grid:
     Has methods for adding ships and guesses 
     and printing the board.
     """
+    def __init__(self, size, num_ships, name, type):
+        self.size = size
+        self.grid = [["." for x in range(size)] for y in range(size)] 
+        self.num_ships = num_ships
+        self.name = name
+        self.type = type
+        self.guesses = []
+        self.ships = []
+
+
